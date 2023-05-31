79eb4ed4a156f58022b9ddffb98a096eba1d4e63
hercules1997 <hercules.chaves.andrade@gmail.com>
2023-05-30 22:28:18 -0300

    Alterando address da api dos files

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -10,7 +10,7 @@ class Product extends Model {
         url: {
           type: Sequelize.VIRTUAL,
           get() {
-            return `http://localhost:3001/product-file/${this.path}`
+            return `https://top-burguer-backend-production.up.railway.app/product-file/${this.path}`
           },
         },
       },
